ec36733caff63110300eb9b0d802ed77bd8ece29
王浩然 <309568486@qq.com>
2017-01-23 16:15:46 +0800

    日志的内容修改

--- githubAction.php
+++ githubAction.php
@@ -12,11 +12,11 @@ abstract class githubAction{
     protected $listenBranch = array();
     public function run(){
         if(empty($this->listenBranch)){
-//            exit;
+            exit;
         }
         $response = json_decode(file_get_contents('php://input'));
         if(!in_array($response->ref,$this->listenBranch)){
-//            exit;
+            exit;
         }
         if(!is_dir('../metaPHPCacheFile')){
             mkdir('../metaPHPCacheFile');
@@ -24,9 +24,7 @@ abstract class githubAction{
         $webRootDir = dirname(dirname(__FILE__));
         $cachePath = $webRootDir.'/metaPHPCacheFile';
         //写日志
-        file_put_contents($cachePath.'/githubReceive.log',$response->ref."\n",FILE_APPEND);
-        //system()
-//        $githubListenClass = classAction::createClass('githubListenClass',$cachePath.'/githubListen.php','temp');
-//        print_r($githubListenClass->save());exit;
+        $result = exec('cd ' . $webRootDir . ';git pull');
+        file_put_contents($cachePath.'/githubReceive.log',$result."\n",FILE_APPEND);
     }
 }
