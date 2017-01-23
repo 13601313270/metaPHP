390b42274fb6ffbe9dab35e5c9e763a8a900f4d9
王浩然 <309568486@qq.com>
2017-01-23 16:42:11 +0800

    变量进行了提取

--- githubAction.php
+++ githubAction.php
@@ -9,22 +9,43 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL ^ E_NOTICE);
 include_once('classAction.php');
 abstract class githubAction{
+    public $webRootDir = '';
+    public $cachePath = '';
     protected $listenBranch = array();
+    //创建分支
+    public function createBranch($name,$isChangeNow=true){
+        $result = exec('cd ' . $this->webRootDir . ';git branch '.$name);
+        if($isChangeNow){
+            $result = exec('cd ' . $this->webRootDir . ';git checkout '.$name);
+            $this->log('createBranch','创建分支:['.$name.']并切换到在此分支;结果'.$result);
+        }else{
+            $this->log('createBranch','创建分支:['.$name.'];结果'.$result);
+        }
+    }
+    //切换分支
+//    public function checkout($name){
+//        $result = exec('cd ' . $this->webRootDir . ';git checkout '.$name);
+//        $this->log('createBranch','创建分支:'.$name.';结果'.$result);
+//    }
     public function run(){
         if(empty($this->listenBranch)){
+            exit;
+        }
+        if(empty($this->webRootDir)){
             exit;
         }
         $response = json_decode(file_get_contents('php://input'));
         if(!in_array($response->ref,$this->listenBranch)){
             exit;
         }
-        $webRootDir = dirname(dirname(__FILE__));
-        $cachePath = $webRootDir.'/metaPHPCacheFile';
-        if(!is_dir($cachePath)){
-            mkdir($cachePath);
+        if(!is_dir($this->cachePath)){
+            mkdir($this->cachePath);
         }
         //写日志
-        $result = exec('cd ' . $webRootDir . ';git pull');
-        file_put_contents($cachePath.'/githubReceive.log',$result."\n",FILE_APPEND);
+        $result = exec('cd ' . $this->webRootDir . ';git pull');
+        $this->log('githubReceive',$result);
+    }
+    public function log($type,$message){
+        file_put_contents($this->cachePath.'/'.$type.'.log',$message."\n",FILE_APPEND);
     }
 }
