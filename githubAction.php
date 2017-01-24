5012cd0e83f423d67d647f4598c77019d0aa4173
王浩然 <309568486@qq.com>
2017-01-25 01:39:52 +0800

    增加了日志

--- githubAction.php
+++ githubAction.php
@@ -22,47 +22,49 @@ abstract class githubAction{
         $result = $this->exec('cd ' . $this->webRootDir . ';git branch '.$name);
         if($isChangeNow){
             $result = $this->exec('cd ' . $this->webRootDir . ';git checkout '.$name);
-            $this->log('createBranch','创建分支:['.$name.']并切换到在此分支;结果'.$result);
+            $this->log('createBranch','创建分支:['.$name.']并切换到在此分支;结果'.json_encode($result));
         }else{
-            $this->log('createBranch','创建分支:['.$name.'];结果'.$result);
+            $this->log('createBranch','创建分支:['.$name.'];结果'.json_encode($result));
         }
     }
     //删除分支
     public function deleteBranch($name){
         $result = $this->exec('cd ' . $this->webRootDir . ';git branch -d '.$name);
-        $this->log('deleteBranch','删除分支:['.$name.'];结果'.$result);
+        $this->log('deleteBranch','删除分支:['.$name.'];结果'.json_encode($result));
     }
     //切换分支
     public function checkout($name){
         $result = $this->exec('cd ' . $this->webRootDir . ';git checkout '.$name);
-        $this->log('createBranch','创建分支:'.$name.';结果'.$result);
+        $this->log('createBranch','创建分支:'.$name.';结果'.json_encode($result));
     }
     //合并分支
     public function mergeBranch($branchName){
         $result = $this->exec('cd ' . $this->webRootDir . ';git merge '.$branchName);
-        $this->log('mergeBranch','合并分支:'.$branchName.';结果'.$result);
+        $this->log('mergeBranch','合并分支:'.$branchName.';结果'.json_encode($result));
         $this->commit('合并分支:'.$branchName);
     }
     //更新绑定分支代码
     public function pull(){
         //写日志
         $result = $this->exec('cd ' . $this->webRootDir . ';git checkout '.$this->webRootDir.';git pull;');
-        $this->log('githubReceive',$result);
+        $this->log('githubReceive',json_encode($result));
     }
     //推送代码push
     public function push(){
-        $result = $this->exec('cd ' . $this->webRootDir . ';git push origin '.$this->runLocalBranch);
-        $this->log('mergeBranch','提交代码;结果'.$result);
+        $result = $this->exec('cd ' . $this->webRootDir . ';git push');
+        $this->log('mergeBranch','提交代码;结果'.json_encode($result));
     }
     //提交代码commit
     public function commit($message){
         $result = $this->exec('cd ' . $this->webRootDir . ';git add .;git commit -m "'.$message.'"');
-        $this->log('mergeBranch','提交代码;结果'.$result);
+        $this->log('commit','提交代码;结果'.json_encode($result));
     }
     //执行命令
     private function exec($line){
         $result = array();
-        return exec($line,$result);
+        $return = exec($line,$result);
+        print_r($result);
+        return $result;
     }
     //记录日志
     public function log($type,$message){
