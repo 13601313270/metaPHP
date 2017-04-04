689c3e1c8cd5c2911889a1d53d99571692b503e0
王浩然 <309568486@qq.com>
2017-04-04 10:55:49 +0800

    改为了返回对象，然后增加了parent方法

--- metaSearch.php
+++ metaSearch.php
@@ -33,46 +33,83 @@ class metaSearch{
         return $isSearch;
     }
     private $findArr = array();
-    private function getArrSearchSingle($searchStr, &$arr){
+    private $findArrKey = array();
+    private function getArrSearchSingle($searchStr, &$arr,$searchArr){
         list($search,$filter) = explode(':',$searchStr);
-        if($this->isSearch($arr,$searchStr)){
-            $this->findArr[] = $arr;
-        }elseif($arr[$search]){
+        if($arr[$search]){
             if($filter!=null){
                 if(preg_match('/filter\((.*)\)/',$filter,$match)){
                     if($this->isSearch($arr[$search],$match[1])){
                         $this->findArr[] = &$arr[$search];
+                        $this->findArrKey[] = array_merge($searchArr,array($search));
                     }
                 }
             }else{
                 $this->findArr[] = &$arr[$search];
+                $this->findArrKey[] = array_merge($searchArr,array($search));
             }
         }else{
             foreach ($arr as $key1=>&$value1) {
                 if($this->isSearch($value1,$searchStr)){
                     $this->findArr[] = &$value1;
+                    $this->findArrKey[] = array_merge($searchArr,array($key1));
                 }elseif(is_array($value1)){
-                    $this->getArrSearchSingle($searchStr, $value1);
+                    $this->getArrSearchSingle($searchStr, $value1,array_merge($searchArr,array($key1)));
                 }
             }
         }
-        return $this->findArr;
     }
-    private function getByArr($str,$arrList){
+    private function getByArr($str,$waitAllDom,$baseArrKey){
         $returnArr = array();
-        foreach($arrList as &$dom){
+        $returnArrKey = array();
+        foreach($waitAllDom as $key=>&$dom){
             $this->findArr = array();
-            $returnArr = array_merge($returnArr,$this->getArrSearchSingle($str,$dom));
+            $this->findArrKey = array();
+            $this->getArrSearchSingle($str,$dom,$baseArrKey[$key]);
+            $returnArr = array_merge($returnArr,$this->findArr);
+            $returnArrKey = array_merge($returnArrKey,$this->findArrKey);
         }
-        return $returnArr;
+        return array($returnArr,$returnArrKey);
     }
-    public function search(&$codeArr,$sSearch){
-        $metaSearchApi = new metaSearch();
-        $baseArr = array(&$codeArr);
+    //待运算数组
+    private $codeArr = array();
+    //search运行结果数组对应key数组,每次运行search会重置
+    private $codeResultKey = array();
+    public function __construct(&$codeArr)
+    {
+        $this->codeArr = &$codeArr;
+    }
+    public function search($sSearch){
+        $baseArr = array(&$this->codeArr);
+        $baseArrKey = array(array());
         foreach(explode(' ',$sSearch) as $str){
-            $baseArr =  $metaSearchApi->getByArr($str,$baseArr);
+            $result = $this->getByArr($str,$baseArr,$baseArrKey);
+            $baseArr = $result[0];
+            $baseArrKey = $result[1];
         }
-        return $baseArr;
+        $this->codeResultKey = $baseArrKey;
+        return $this;
     }
-
+    public function parent(){
+        $result = array();
+        foreach($this->codeResultKey as $k=>$v){
+            array_pop($v);
+            if(!in_array($v,$result)){
+                $result[] = $v;
+            }
+        }
+        $this->codeResultKey = $result;
+        return $this;
+    }
+    public function toArray(){
+        $return = array();
+        foreach($this->codeResultKey as $keysItem){
+            $resultItem = &$this->codeArr;
+            foreach($keysItem as $key){
+                $resultItem = &$resultItem[$key];
+            }
+            $return[] = &$resultItem;
+        }
+        return $return;
+    }
 }
